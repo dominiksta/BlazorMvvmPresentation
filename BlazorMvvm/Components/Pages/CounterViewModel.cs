978c39ab7334d1c739b70f7f1d585db6be11c372
Dominik Stahmer <dstahmer@recom.eu>
2026-02-19 21:14:11 +0100

    initial commit

---
+++ BlazorMvvm/Components/Pages/CounterViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace BlazorMvvm.Components.Pages;
+
+public partial class CounterViewModel : ObservableObject
+{
+    public int Count
+    {
+        get;
+        set
+        {
+            SetProperty(ref field, value);
+            IncrementCountCommand.NotifyCanExecuteChanged();
+        }
+    }
+
+    public readonly RelayCommand IncrementCountCommand;
+    
+    public CounterViewModel()
+    {
+        IncrementCountCommand = new RelayCommand(() => Count++, IncrementEnabled);
+    }
+    
+    private bool IncrementEnabled() => Count < 10;
+    
+    // [ObservableProperty]
+    // private int _count;
+    //
+    // [RelayCommand(CanExecute = nameof(IncrementEnabled))]
+    // private void IncrementCount() => Count++;
+}
